6eebb23b9a15173a025ab076492b89eaf937ce00
Your Name <you@example.com>
2024-12-16 06:13:02 -0800

    upload index.php project

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo " <table border=1> ";
+for ($i=1;$i<10;$i++)
+{
+    each "<tr>"
+    for ($j=1;$j<10;$j++)
+    {
+        echo "<td>".$i*Sj. "</td>";
+    }
+
+    echo "</tr>";
+
+}
+echo "</table>"
+
+?>
